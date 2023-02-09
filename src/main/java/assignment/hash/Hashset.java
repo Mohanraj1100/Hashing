9190eef3fc896a5f7abcf44025a900df03509ba0
Mohanraj1100 <123471382+Mohanraj1100@users.noreply.github.com>
2023-02-09 18:23:13 +0530

    change

--- src/main/java/assignment/hash/Hashset.java
+++ src/main/java/assignment/hash/Hashset.java
@@ -8,30 +8,30 @@ public class Hashset {
 
 	public static void main(String[] args) {
 		HashSet<String> hs = new HashSet<String>();
-		Logger log=Logger.getLogger("hi");
+		Logger log1=Logger.getLogger("hi");
 		hs.add(5);
 		hs.add(50.23);
 		hs.add(500);
 		hs.add(52);
 		hs.add(51);
 		hs.add(5);
-		log.info("Elements are"+String.valueOf(hs));
+		log1.log(Level.INFO,()->"Elements are"+hs);
 		hs.remove(51);
-		log.info("After removing the elements are"+String.valueOf(hs));
-		log.info("Size of Hashset is "+String.valueOf(hs.size())); 
-		LinkedList ll = new LinkedList();
+		log1.log(Level.INFO,()->"After removing the elements are"+hs);
+		log1.log(Levelo.INFO,()->"Size of Hashset is "+ hs.size()); 
+		LinkedList<String> ll = new LinkedList<String>();
 		ll.add(23);
 		ll.add(45);
 		ll.add("hi");
 		ll.add('c');
 		ll.add(23);
-		log.info("Values in LinkedList"+String.valueOf(ll));
+		log1.log(Level.INFO,()-> "Values in LinkedList"+ ll);
 		
 		HashSet<String> hs1 = new HashSet<String>(ll);
-		log.info("Values after adding linkedlist elements"+String.valueOf(hs1));
+		log1.log(Level.INFO,()-> "Values after adding linkedlist elements"+ hs1);
 		hs1.clear();
-		log.info("Values after cleared"+String.valueOf(hs1));
-		log.info("Size after cleared  "+String.valueOf(hs1.size()));
+		log1.log(Level.INFO,()-> "Values after cleared"+ hs1);
+		log1.log(Level.INFO,()-> "Size after cleared  "+ hs1.size());
 		
 		
 		
