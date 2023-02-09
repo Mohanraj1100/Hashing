31692b6fec28bde8475f3128fa8b3fd559336120
Mohanraj1100 <123471382+Mohanraj1100@users.noreply.github.com>
2023-02-09 18:13:35 +0530

    Update Hash.java

--- src/main/java/assignment/hash/Hash.java
+++ src/main/java/assignment/hash/Hash.java
@@ -6,18 +6,18 @@ public class Hash
 {
     public static void main( String[] args )
     {
-        HashMap has = new HashMap();
+        HashMap<String,Integer> has = new HashMap<String,Integer>();
         Logger log = Logger.getLogger("hi");
         has.put("Vaigai EXP", "06:30");
         has.put("Pandian EXP","07:00");
         has.put("Chennai EXP","19:00");
         log.info("Entries are"+String.valueOf(has));
         log.info(String.valueOf(has.get("Vaigai EXP")));
-        HashMap has2 = new HashMap();
+        HashMap<String,Integer> has2 = new HashMap<String,Integer>();
         has2.putAll(has);
         log.info("Values after adding another hashmap values"+String.valueOf(has2));
         log.info("Size is "+String.valueOf(has2.size()));
-        Set s = has.entrySet();
+        Set<?> s = has.entrySet();
         log.info("Print the keys"+String.valueOf(has.keySet()));
         log.info("Print the values"+String.valueOf(has.values()));
         has2.clear();
